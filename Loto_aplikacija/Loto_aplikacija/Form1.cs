534a7df15dc512f587b921e397dd7f488ea9900b
dhorvat2 <dhorvat2@foi.hr>
2015-03-31 22:52:55 +0200

    Unijet kod
    Aplikacija izbacuje brojeve lota

--- Loto_aplikacija/Loto_aplikacija/Form1.cs
+++ Loto_aplikacija/Loto_aplikacija/Form1.cs
@@ -12,9 +12,52 @@ namespace Loto_aplikacija
 {
     public partial class Form1 : Form
     {
+
+        private Loto loto;
+       
         public Form1()
         {
             InitializeComponent();
+            loto=new Loto();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<string> vrijednosti = new List<string>(); 
+            vrijednosti.Add(textBox1.Text);
+            vrijednosti.Add(textBox2.Text);
+            vrijednosti.Add(textBox3.Text);
+            vrijednosti.Add(textBox4.Text);
+            vrijednosti.Add(textBox5.Text);
+            vrijednosti.Add(textBox6.Text);
+            vrijednosti.Add(textBox7.Text);
+
+            bool ispravnaKombinacija = loto.UnesiUplaceneBrojeve(vrijednosti);
+            if (ispravnaKombinacija == true) {
+                button2.Enabled = true; 
+            } 
+            else { 
+                button2.Enabled = false; 
+                MessageBox.Show("Kombinacija uplaćenih brojeva nije ispravna!"); }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loto.GenerirajDobitnuKombinaciju(); 
+            textBox8.Text = loto.DobitniBrojevi[0].ToString();
+            textBox9.Text = loto.DobitniBrojevi[1].ToString();
+            textBox10.Text = loto.DobitniBrojevi[2].ToString();
+            textBox11.Text = loto.DobitniBrojevi[3].ToString();
+            textBox12.Text = loto.DobitniBrojevi[4].ToString();
+            textBox13.Text = loto.DobitniBrojevi[5].ToString();
+            textBox14.Text = loto.DobitniBrojevi[6].ToString(); 
+            int brojPogodaka = loto.IzracunajBrojPogodaka(); 
+            label2.Text = brojPogodaka.ToString();
+        }
+
+        
+
+        
     }
 }
